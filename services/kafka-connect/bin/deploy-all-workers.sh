ba2b97b8d1fe46bae640d2bbb5398fea0e39e5e4
lorandvatamany <lorand.vatamany@aureliusenterprise.com>
2024-06-18 08:29:22 +0200

    Automaticall publish kafka connect workers on startup

---
+++ services/kafka-connect/bin/deploy-all-workers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Default values
+DEFAULT_WORKER_DIR="/tmp/aurelius/workers"
+DEFAULT_KAFKA_CONNECT_URL="http://localhost:${CONNECT_REST_PORT}"
+
+WORKER_DIR=${1:-$DEFAULT_WORKER_DIR}
+KAFKA_CONNECT_URL=${2:-$DEFAULT_KAFKA_CONNECT_URL}
+
+echo "Deploying all workers in $WORKER_DIR to $KAFKA_CONNECT_URL..."
+
+# Loop through all JSON files in the worker directory
+for file in "$WORKER_DIR"/*.json; do
+    # Update the worker
+    /tmp/aurelius/bin/deploy-worker.sh "$file" "$KAFKA_CONNECT_URL"
+done
+
+echo "Done deploying all workers"
