2c8c5d3625cabe0948fd20db7f7f572deeb3e8d7
lorandvatamany <lorand.vatamany@aureliusenterprise.com>
2024-06-19 14:13:57 +0200

    Suggested changes

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,22 +3,23 @@
 # Add current directory as safe repository location
 git config --global --add safe.directory $PWD
 
-# Set node modules permissions
-sudo chown -R $(whoami) /workspace/node_modules
-sudo chmod -R 755 /workspace/node_modules
-
 # Install dependencies
 npm install
 poetry install --no-root
 
+JARS_DIR=backend/m4i-flink-jobs/m4i_flink_jobs/jars
+
 # Download JAR files if not already present
-JARS_DIR=/backend/m4i-flink-jobs/m4i_flink_jobs/jars/
-if [ -d "$JARS_DIR" ]; then
-    echo "Directory $DIRECTORY exists."
-else
-    wget -P "$JARS_DIR" "https://repo.maven.apache.org/maven2/org/apache/flink/flink-sql-connector-elasticsearch7/3.0.1-1.17/flink-sql-connector-elasticsearch7-3.0.1-1.17.jar"
-    wget -P "$JARS_DIR" "https://repo.maven.apache.org/maven2/org/apache/flink/flink-sql-connector-kafka/1.17.1/flink-sql-connector-kafka-1.17.1.jar"
-fi
+while read -r url; do
+    [ -z "$url" ] && continue
+    filename=$(basename "$url")
+    if [ -e "$JARS_DIR/$filename" ]; then
+        echo "File jars/$filename already exists, skipping download."
+        continue
+    fi
+
+    wget -P "$JARS_DIR/" "$url"
+done < "$JARS_DIR/manifest"
 
 # set permissions
 sudo chmod -R 777 /opt/flink/log
