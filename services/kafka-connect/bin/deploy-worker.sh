fd0cd0109729121ba88764e68c42d259b1f61ac9
lorandvatamany <lorand.vatamany@aureliusenterprise.com>
2024-12-12 13:36:47 +0000

    Fix helmchart

--- services/kafka-connect/bin/deploy-worker.sh
+++ services/kafka-connect/bin/deploy-worker.sh
@@ -12,22 +12,22 @@ if [ -z "$WORKER_FILE" ]; then
     exit 1
 fi
 
-# Check if ELASTICSEARCH_URL is set
-if [[ -n "$ELASTICSEARCH_URL" ]]; then
-  sed -i.bak "s#\"connection.url\": \".*\"#\"connection.url\": \"$ELASTICSEARCH_URL\"#" "$JSON_FILE"
-  echo "Replaced connection.url with ELASTICSEARCH_URL value: $ELASTICSEARCH_URL"
-fi
-# Check if kafka_connect_elastic_password is set
-if [[ -n "$KAFKA_CONNECT_ELASTIC_PASSWORD" ]]; then
-  sed -i.bak "s#\"connection.password\": \".*\"#\"connection.password\": \"$KAFKA_CONNECT_ELASTIC_PASSWORD\"#" "$JSON_FILE"
-  echo "Replaced connection.password with ELASTIC_PASSWORD value."
-fi
-
 # Derive the worker name from the file name
 NAME=$(basename "$WORKER_FILE" .json)
 
 # Read the file content
 WORKER=$(cat "$WORKER_FILE")
+
+# Check if ELASTICSEARCH_URL is set
+if [[ -n "$ELASTICSEARCH_URL" ]]; then
+  WORKER=$(echo "$WORKER" | sed "s#\"connection.url\": \".*\"#\"connection.url\": \"$ELASTICSEARCH_URL\"#")
+  echo "Replaced connection.url with ELASTICSEARCH_URL value: $ELASTICSEARCH_URL"
+fi
+# Check if kafka_connect_elastic_password is set
+if [[ -n "$KAFKA_CONNECT_ELASTIC_PASSWORD" ]]; then
+  WORKER=$(echo "$WORKER" | sed "s#\"connection.password\": \".*\"#\"connection.password\": \"$KAFKA_CONNECT_ELASTIC_PASSWORD\"#")
+  echo "Replaced connection.password with ELASTIC_PASSWORD value."
+fi
 
 echo "Deploying worker $NAME to $KAFKA_CONNECT_URL/connectors/$NAME/config"
 
