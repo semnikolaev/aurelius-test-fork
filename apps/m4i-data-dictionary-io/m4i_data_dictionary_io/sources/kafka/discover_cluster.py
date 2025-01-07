4d18df526959fee055857ecbba9d77cdab74eda0
lorandvatamany <lorand.vatamany@aureliusenterprise.com>
2025-01-07 15:36:19 +0100

    Read kafka sources test

--- apps/m4i-data-dictionary-io/m4i_data_dictionary_io/sources/kafka/discover_cluster.py
+++ apps/m4i-data-dictionary-io/m4i_data_dictionary_io/sources/kafka/discover_cluster.py
@@ -11,15 +11,7 @@ import random
 from m4i_atlas_core.config.config_store import ConfigStore
 
 
-# Configuration parameters
-store = ConfigStore.get_instance()
-SCHEMA_REGISTRY_URL = store.get("schema_registry_url")
-
-# Initialize Schema Registry client
-if SCHEMA_REGISTRY_URL:
-    schema_registry_client = SchemaRegistryClient({'url': SCHEMA_REGISTRY_URL})
-
-def get_internal_topic_names(bootstrap_servers: str) -> List[str]:
+def get_external_topic_names(bootstrap_servers: str) -> List[str]:
     """
     Retrieve topic names for the given cluster ID,
     filtering out internal and default topics.
@@ -29,10 +21,10 @@ def get_internal_topic_names(bootstrap_servers: str) -> List[str]:
     # Get all topics, and then filter for internal topics
     all_topics = [str(t) for t in admin_client.list_topics().topics.values()]
     futureMap = admin_client.describe_topics(TopicCollection(all_topics))
-    internal_topics = [topic_name for topic_name, future in futureMap.items() if not (t := future.result()).is_internal]
+    external_topics = [topic_name for topic_name, future in futureMap.items() if not (t := future.result()).is_internal]
     return [
         topic
-        for topic in internal_topics
+        for topic in external_topics
         if not topic.endswith(("-offsets", "-status", "-configs"))
     ]
 
@@ -42,7 +34,7 @@ def is_avro(message) -> bool:
     return message and message[0] == 0
 
 
-def deserialize_avro(message):
+def deserialize_avro(message: bytes, schema_registry_client: SchemaRegistryClient):
     """Deserialize an Avro message using the Schema Registry."""
     schema_id = int.from_bytes(message[1:5], byteorder='big')
     avro_schema = schema_registry_client.get_schema(schema_id).schema_str
@@ -53,13 +45,17 @@ def deserialize_avro(message):
     return reader.read(decoder)
 
 
-def consume_messages(topic_names: List[str], bootstrap_servers: str):
+def consume_messages(
+    topic_names: List[str],
+    bootstrap_servers: str,
+    schema_registry_client: Union[SchemaRegistryClient, None],
+    consumer_group_id_prefix: str):
     """Consume messages from the provided topic names and parse them."""
     topic_data = []
     for topic in topic_names:
         consumer = Consumer({
             'bootstrap.servers': bootstrap_servers,
-            'group.id': f'{store.get("consumer_group_id_prefix")}-{random.randint(1, 1000)}',
+            'group.id': f'{consumer_group_id_prefix}-{random.randint(1, 1000)}',
             'auto.offset.reset': 'earliest'
         })
 
@@ -71,9 +67,11 @@ def consume_messages(topic_names: List[str], bootstrap_servers: str):
                 print("No message received")
             elif msg.error():
                 print(f"Error while consuming message: {msg.error()}")
+            elif msg.value() is None:
+                print(f"Message in topic {msg.topic()} has no value")
             else:
                 message_bytes = msg.value()
-                value = parse_message(message_bytes, msg.topic())
+                value = parse_message(message_bytes, msg.topic(), schema_registry_client)
                 if value is not None:
                     topic_data.append({
                         "name": topic,
@@ -84,7 +82,7 @@ def consume_messages(topic_names: List[str], bootstrap_servers: str):
     return topic_data
 
 
-def parse_message(message_bytes, topic) -> Union[Dict[str, Any], Any]:
+def parse_message(message_bytes: bytes, topic, schema_registry_client) -> Union[Dict[str, Any], Any]:
     """Attempt to parse the message as JSON or Avro."""
     try:
         value = json.loads(message_bytes.decode('utf-8'))
@@ -93,7 +91,7 @@ def parse_message(message_bytes, topic) -> Union[Dict[str, Any], Any]:
     except (UnicodeDecodeError, json.decoder.JSONDecodeError):
         if is_avro(message_bytes):
             try:
-                value = deserialize_avro(message_bytes)
+                value = deserialize_avro(message_bytes, schema_registry_client)
                 print(f"Topic: {topic}, Avro message: {value}")
                 return value
             except Exception as avro_error:
@@ -103,11 +101,21 @@ def parse_message(message_bytes, topic) -> Union[Dict[str, Any], Any]:
     return None
 
 
-def discover_cluster() -> List[Dict[str, Any]]:
+def discover_cluster(store: ConfigStore) -> List[Dict[str, Any]]:
     """Main function to execute the Kafka topic message consumption process."""
+    # Get configurations
     bootstrap_servers = store.get("bootstrap_servers")
-    topic_names = get_internal_topic_names(bootstrap_servers)
+    consumer_prefix = store.get("consumer_group_id_prefix")
+    schema_registry_client = SchemaRegistryClient({'url': store.get("schema_registry_url")}) if store.get("schema_registry_url") else None
+    # Retrieve the external topic names
+    topic_names = get_external_topic_names(bootstrap_servers)
     print("Topics:", topic_names)
-    topic_data = consume_messages(topic_names, bootstrap_servers)
+    # Consume a message from each topic
+    topic_data = consume_messages(
+      topic_names,
+      bootstrap_servers,
+      schema_registry_client,
+      consumer_prefix
+    )
     print("Topic Data:", topic_data)
     return topic_data
